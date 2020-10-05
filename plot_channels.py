89390196c5369e137fb84e5fb438653817b62fd5
leoribg <leonardo.ribeiro@gee.inatel.br>
2020-10-05 07:23:03 -0700

    Set y top

--- plot_channels.py
+++ plot_channels.py
@@ -6,12 +6,12 @@ y = []
 freq_label = []
 color_array = []
 
-with open('plot_data.csv','r') as csvfile:
+with open('meas.txt','r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x.append(int(row[0]))
-        y.append((120 - abs(int(row[1]))))
-        if int(row[1]) < -90 :
+        y.append((120 - abs(float(row[1]))))
+        if float(row[1]) < -90 :
                 color_array.append('green')
         else :
                 color_array.append('red')
@@ -20,6 +20,7 @@ print(color_array)
 
 freq_label = x
 plt.bar(x, y, bottom=-120, tick_label=freq_label, width=0.8, color=color_array)
+plt.ylim(top=0)
 plt.xlabel('Frequency')
 plt.ylabel('dBm')
 plt.title('Power Level from measured channels\nWi-SUN')
