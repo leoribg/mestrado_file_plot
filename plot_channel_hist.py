ab5ca27887f94225faf1a284b3d421d8580b4694
leoribg <leonardo.ribeiro@gee.inatel.br>
2021-01-31 21:15:43 -0300

    Update plot_channel_hist.py

--- plot_channel_hist.py
+++ plot_channel_hist.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import csv
+import numpy as np
+import scipy.stats
 
 x = []
 
@@ -8,9 +10,13 @@ with open('9022.txt','r') as csvfile:
     for row in plots:
         x.append((float(row[0])))
 
-plt.hist(x, bins=[-100,-95,-90,-85,-80,-75,-70,-65,-60,-55,-50,-45,-40,-35,-30,-25,-20,-15,-10,-5,0])
+mean = np.mean(x)
+result = scipy.stats.describe(x, ddof=1, bias=False)
+print(result)
+
+plt.hist(x, bins=np.linspace(result.minmax[0] - 3, result.minmax[0] + 3, num=300))#[-100,-95,-90,-85,-80,-75,-70,-65,-60,-55,-50,-45,-40,-35,-30,-25,-20,-15,-10,-5,0])
 plt.xlabel('Level (dBm)')
-plt.ylabel('Frequency')
+plt.ylabel('Occurrence')
 plt.title('Histogram from channel\n')
 #plt.legend()
 plt.show()
